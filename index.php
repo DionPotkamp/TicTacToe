651f1df96c167267a46c08ed39c6d8fcde557b1e
DionPotkamp <dion.potkamp@gmail.com>
2019-08-23 18:43:31 +0200

    Fixed indent mistake, used icon i/0 text

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <!-- Script for fontawesome (used for in-game symbols and settings menu) -->
-    <script src="https://kit.fontawesome.com/<<%¥|π×>>.js"></script>
+    <script src="https://kit.fontawesome.com/.js"></script>
     
     <title>Tic Tac Toe</title>
 </head>
@@ -69,27 +69,25 @@
 <!-- Modal for the settings -->
 <div class="modal fade" id="settings" tabindex="-1" role="dialog" aria-labelledby="settingsLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="settingsLabel">Settings</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-            <button type="button" id='speed' class="btn btn-info btn-lg btn-block" onclick="autoplaySpeed()">Set&nbsp;autoplay&nbsp;speed</button>
-            <button type="button" id='iteration' class="btn btn-info btn-lg btn-block" onclick="autoplayIt()">Set&nbsp;autoplay&nbsp;iterations</button>
-            <button type="button" id='score' class="btn btn-info btn-lg btn-block" onclick="autoplayScore()">Add&nbsp;autoplay&nbsp;to&nbsp;score</button>
-            <button type="button" class="btn btn-warning btn-lg btn-block" onclick="if (confirm('Are you sure?')) resetScores()">Reset&nbsp;scores</button>
-        
-      </div>
-      <div class="modal-footer">
-          <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
-      </div>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="settingsLabel">Settings</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fas fa-times"></i></span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <button type="button" id='speed' class="btn btn-info btn-lg btn-block" onclick="autoplaySpeed()">Set&nbsp;autoplay&nbsp;speed</button>
+                <button type="button" id='iteration' class="btn btn-info btn-lg btn-block" onclick="autoplayIt()">Set&nbsp;autoplay&nbsp;iterations</button>
+                <button type="button" id='score' class="btn btn-info btn-lg btn-block" onclick="autoplayScore()">Add&nbsp;autoplay&nbsp;to&nbsp;score</button>
+                <button type="button" class="btn btn-warning btn-lg btn-block" onclick="if (confirm('Are you sure?')) resetScores()">Reset&nbsp;scores</button>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
-
 
 <!-- JS for the game -->
 <script src="game.js"></script>
