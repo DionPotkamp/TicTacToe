00fc82c86532ca904ffd531eb4bd7c22b9a5a600
dion.potkamp <dion.potkamp@webstores.nl>
2019-09-25 14:41:12 +0200

    added gitignore,cleaned up code, rename to html so no php server is required

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ board = [
     ['', '', '']
 ];
 /**
- * positions: all the available spots on the board. First letter = row, second = collum
+ * positions: all the available spots on the board. First letter = row, second = column
  */
 positions = [
     "00", "01", "02", 
@@ -25,7 +25,7 @@ autoPlayDelay = 250;
 document.getElementById('speed').innerHTML = 'Set&nbsp;autoplay&nbsp;speed ('+autoPlayDelay+')';
 function autoplaySpeed() {
     autoPlayDelay = parseInt(prompt('Give the delay in milliseconds'));
-    if (autoPlayDelay == null || autoPlayDelay == "") {
+    if (autoPlayDelay === null || autoPlayDelay === "") {
         autoPlayDelay = 250;
     } else {
         document.getElementById('speed').innerHTML = 'Set&nbsp;autoplay&nbsp;speed ('+autoPlayDelay+')';
@@ -39,7 +39,7 @@ autoPlayIterations = 10;
 document.getElementById('iteration').innerHTML = 'Set&nbsp;autoplay&nbsp;iterations ('+autoPlayIterations+')';
 function autoplayIt() {
     autoPlayIterations = parseInt(prompt('Give the number of inerations'));
-    if (autoPlayIterations == null || autoPlayIterations == "") {
+    if (autoPlayIterations === null || autoPlayIterations === "") {
         autoPlayIterations = 10;
     } else {
         document.getElementById('iteration').innerHTML = 'Set&nbsp;autoplay&nbsp;iterations ('+autoPlayIterations+')';
@@ -53,7 +53,7 @@ autoPlayAddWin = false;
 document.getElementById('score').innerHTML = 'Add&nbsp;autoplay&nbsp;to&nbsp;score ('+autoPlayAddWin+')';
 function autoplayScore() {
     autoPlayAddWin = confirm('Add autoplay wins to score. Choose OK to set to true, Choose cancel to set to false');
-    if (autoPlayAddWin == null || autoPlayAddWin == "") {
+    if (autoPlayAddWin === null || autoPlayAddWin === "") {
         autoPlayAddWin = false;
         document.getElementById('score').innerHTML = 'Add&nbsp;autoplay&nbsp;to&nbsp;score ('+autoPlayAddWin+')';
     } else {
@@ -86,16 +86,16 @@ function setNewPlayer() {
 
 /**
  * Switch to the other player
- * @return boolean, if the execution was a succes or not.
+ * @return boolean, if the execution was a success or not.
  */
 function switchPlayer() {
-    if (CurrentPlayer == "") {
+    if (CurrentPlayer === "") {
         return false;
-    } else if (CurrentPlayer == "X") {
+    } else if (CurrentPlayer === "X") {
         CurrentPlayer = "O";
         document.getElementById('text').innerHTML = "Current player is " + CurrentPlayer;
         return true;
-    } else if (CurrentPlayer == "O") {
+    } else if (CurrentPlayer === "O") {
         CurrentPlayer = "X";
         document.getElementById('text').innerHTML = "Current player is " + CurrentPlayer;
         return true;
@@ -106,13 +106,13 @@ function switchPlayer() {
 
 /**
  * Place the X or O symbol in the html on the given position
- * @return boolean, if the execution was a succes or not.
+ * @return boolean, if the execution was a success or not.
  */
 function place(position) {
-    if (CurrentPlayer == "X") {
+    if (CurrentPlayer === "X") {
         document.getElementById(position).innerHTML = PlayerX;
         return true;
-    } else if (CurrentPlayer == "O") {
+    } else if (CurrentPlayer === "O") {
         document.getElementById(position).innerHTML = PlayerO;
         return true;
     } else {
@@ -123,15 +123,15 @@ function place(position) {
 
 /**
  * Check in array board if the given position is available or not
- * @return boolean, if the execution was a succes or not.
+ * @return boolean, if the execution was a success or not.
  */
 function checkIfAvailable(position) {
     positionArray = position.split("");
     var boardPos = board[positionArray[0]][positionArray[1]];
 
-    if(boardPos == "") {
+    if(boardPos === "") {
         return true;
-    } else if (boardPos == "X" || boardPos == "O") {
+    } else if (boardPos === "X" || boardPos === "O") {
         return false;
     } else {
         return false;
@@ -142,12 +142,12 @@ function checkIfAvailable(position) {
 /**
  * Occupies the position in array board with the current player
  * And removes the position in the array positions
- * @return boolean, if the execution was a succes or not.
+ * @return boolean, if the execution was a success or not.
  */
 function putInBoard(position) {
     var positionArray = position.split("");
 
-    if (CurrentPlayer == "X" || CurrentPlayer == "O") {
+    if (CurrentPlayer === "X" || CurrentPlayer === "O") {
         if (board[positionArray[0]][positionArray[1]] = CurrentPlayer) {
             positions.splice(positions.indexOf(position), 1);
             return true;
@@ -164,7 +164,7 @@ function putInBoard(position) {
  * @return boolean, if the execution was a succes or not.
  */
 function drawWin(type, cell) {
-    if (type == 'horizontal') {
+    if (type === 'horizontal') {
         one = [cell]+[0];
         two = [cell]+[1];
         three = [cell]+[2];
@@ -174,7 +174,7 @@ function drawWin(type, cell) {
         
         document.getElementById('text').innerHTML = CurrentPlayer + " won the game!";
         return true;
-    } else if (type == 'vertical') {
+    } else if (type === 'vertical') {
         one = [0]+[cell];
         two = [1]+[cell];
         three = [2]+[cell];
@@ -184,21 +184,21 @@ function drawWin(type, cell) {
         
         document.getElementById('text').innerHTML = CurrentPlayer + " won the game!";
         return true;
-    } else if (type == 'diaL-R') {
+    } else if (type === 'diaL-R') {
         document.getElementById('00').style.color = 'rgb(34, 221, 34)';
         document.getElementById('11').style.color = 'rgb(34, 221, 34)';
         document.getElementById('22').style.color = 'rgb(34, 221, 34)';
         
         document.getElementById('text').innerHTML = CurrentPlayer + " won the game!";
         return true;
-    } else if (type == 'diaR-L') {
+    } else if (type === 'diaR-L') {
         document.getElementById('02').style.color = 'rgb(34, 221, 34)';
         document.getElementById('11').style.color = 'rgb(34, 221, 34)';
         document.getElementById('20').style.color = 'rgb(34, 221, 34)';
         
         document.getElementById('text').innerHTML = CurrentPlayer + " won the game!";
         return true;
-    } else if (type == 'draw') {
+    } else if (type === 'draw') {
         ColorThese = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
         ColorThese.forEach(one);
         function one(value) {
@@ -212,25 +212,22 @@ function drawWin(type, cell) {
 }
 
 /**
- * Checks if the three given values are equal to eachother
+ * Checks if the three given values are equal to each other
  * @return boolean, if the values are equal or not.
  */
 function equal(a, b, c) {
-    if (a == b && a == c && a != '') {
-        return true;
-    } else {
-        return false;
-    }
+    return a === b && a === c && a !== '';
 }
 
 /**
  * Checks if the current player made a winning move
  * if so, it will add a win and call function drawWin
- * @return boolean, if the execution was a succes or not.
+ * @return boolean, if the execution was a success or not.
  */
 function checkWinner() {
-    //horizontal
-    for(var i = 0; i < board.length; i++) {
+    let i;
+//horizontal
+    for(i = 0; i < board.length; i++) {
         if (equal(board[i][0], board[i][1], board[i][2])) {
             winner = CurrentPlayer;
             addWin();
@@ -240,7 +237,7 @@ function checkWinner() {
         }
     }
     //vertical
-    for(var i = 0; i < board.length; i++) {
+    for(i = 0; i < board.length; i++) {
         if (equal(board[0][i], board[1][i], board[2][i])) {
             winner = CurrentPlayer;
             addWin();
@@ -279,13 +276,13 @@ function checkWinner() {
 
 /**
  * If the player wins this round it will add one to the score
- * @return boolean, if the execution was a succes or not.
+ * @return boolean, if the execution was a success or not.
  */
 function addWin() {
-    if (CurrentPlayer == "X") {
+    if (CurrentPlayer === "X") {
         PlayerXWins++;
         return true;
-    } else if (CurrentPlayer == "O") {
+    } else if (CurrentPlayer === "O") {
         PlayerOWins++;
         return true;
     } else {
@@ -295,16 +292,16 @@ function addWin() {
 
 /**
  * Resets all of the variables
- * @return boolean, if the execution was a succes or not.
+ * @return boolean, if the execution was a success or not.
  */
 function restartGame() {    
     board = [['', '', ''],['', '', ''],['', '', '']];
     positions = ["00", "01", "02", "10", "11", "12", "20", "21", "22"];
     positionArray = "";
 
-    if(winner == 'X') {
+    if(winner === 'X') {
         CurrentPlayer = 'O';
-    } else if(winner == 'O') {
+    } else if(winner === 'O') {
         CurrentPlayer = 'X';
     }
     document.getElementById('text').innerHTML = 'Starting player is: ' + CurrentPlayer;
@@ -324,7 +321,7 @@ function restartGame() {
 
 /**
  * Resets all of the scores
- * @return boolean, if the execution was a succes or not.
+ * @return boolean, if the execution was a success or not.
  */
 function resetScores() {
     
@@ -353,19 +350,19 @@ function coreAutoPlay() {
 
 /**
  * The logic of the game, puts all of the functions together
- *      restarts the game when necesary
+ *      restarts the game when necessary
  * @return void, to break out of the loop.
  */
 function autoPlay() {
-    if (autoPlayI == autoPlayIterations) {
+    if (autoPlayI === autoPlayIterations) {
         autoPlayI = 0;
         isAutoPlaying = false;
         return;
     }
     if (winner != null) restartGame();
-    if(positions === undefined || positions.length == 0) restartGame();
+    if(positions === undefined || positions.length === 0) restartGame();
 
-    position = positions[Math.floor(Math.random() * positions.length)];
+    let position = positions[Math.floor(Math.random() * positions.length)];
     
     if (checkIfAvailable(position)) {
         if (putInBoard(position)) {
@@ -382,29 +379,21 @@ function autoPlay() {
                 } else {
                     if (switchPlayer()) {
                         setTimeout(autoPlay, autoPlayDelay);
-                    } else {
-                        return;
                     }
                 }
-            } else {
-                return;
             }
-        } else {
-            return;
         }
-    } else {
-        return;
     }
 }
 
 /**
  * The logic of the game, puts all of the functions together
- *      restarts the game when necesary
- * @return boolean, if the execution was a succes or not.
+ *      restarts the game when necessary
+ * @return boolean, if the execution was a success or not.
  */
 function core(position) {
     if (winner != null) restartGame();
-    if(positions === undefined || positions.length == 0) restartGame();
+    if(positions === undefined || positions.length === 0) restartGame();
 
     if (checkIfAvailable(position)) {
         if (putInBoard(position)) {
@@ -413,27 +402,13 @@ function core(position) {
                     document.getElementById('Xwins').innerHTML = PlayerXWins;
                     document.getElementById('Owins').innerHTML = PlayerOWins;
                     document.getElementById('draws').innerHTML = PlayerDraws;
-                    return;
                 } else {
-                    if (switchPlayer()) {
-                        
-                    } else {
-                        return;
-                    }
+                    switchPlayer()
                 }
-            } else {
-                return;
             }
-        } else {
-            return;
         }
-    } else {
-        return;
     }
 }
-
-
-
 
 // -------------------------------
 //             TO-DO 
